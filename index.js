78a259366789f991d32a0f63135dd370e0452bb3
brightonchege <brightonspak@gmail.com>
2022-08-19 01:09:47 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    // return `${customerName.toUpperCase()}`
+    customerName = customerName.toUpperCase()
+return customerName
+    
+}
+
+var bestCustomer 
+function setBestCustomer(){
+    return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer ='maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+    const LeastFavoriteCustomer = "chege"
+    LeastFavoriteCustomer = 'brighton'
+}
